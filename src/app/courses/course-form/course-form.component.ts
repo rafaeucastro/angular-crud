6dd5ebb6de8511ec4c2360d9ba73165eca826cee
Rafaeu Castro <castrorafael456@gmail.com>
2024-06-20 18:08:09 -0300

    Validação dos campos do formulario

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,11 +1,11 @@
 import { ActivatedRoute, Router } from '@angular/router';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { NonNullableFormBuilder } from '@angular/forms';
 import { first } from 'rxjs';
 
 @Component({
@@ -16,10 +16,13 @@ import { first } from 'rxjs';
 export class CourseFormComponent {
   form = this.formBuilder.group({
     id: [''],
-    name: [''],
-    category: [''],
+    name: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.max(100)],
+    ],
+    category: ['', [Validators.required]],
   });
-  categories: string[] = ['back-end', 'front-end'];
+  categories: string[] = ['Back-end', 'Front-end'];
 
   constructor(
     private snackBar: MatSnackBar,
@@ -35,27 +38,29 @@ export class CourseFormComponent {
   }
 
   onSubmit() {
+    if(this.form.invalid) return;
+
     this.service
       .save(this.form.value)
-      .subscribe({ next: this.onSaveSuccess, error: this.onSaveError});
+      .subscribe({ next: this.onSaveSuccess, error: this.onSaveError });
   }
 
   openSnackBar(message: string) {
-    this.snackBar.open(message, "Fechar", { duration: 3000 });
+    this.snackBar.open(message, 'Fechar', { duration: 3000 });
   }
 
-  onSaveSuccess = () =>{
+  onSaveSuccess = () => {
     this.router.navigate(['courses']);
-    this.openSnackBar("Curso salvo com sucesso!");
-  }
+    this.openSnackBar('Curso salvo com sucesso!');
+  };
 
   onSaveError = (error: HttpErrorResponse) => {
-    this.openSnackBar("Não foi possível salvar o curso!");
-  }
+    this.openSnackBar('Não foi possível salvar o curso!');
+  };
 
   onLoadError = () => {
-    this.openSnackBar("Não foi possível carregar o curso!");
-  }
+    this.openSnackBar('Não foi possível carregar o curso!');
+  };
 
   onCancel() {
     this.router.navigate(['courses']);
@@ -67,5 +72,24 @@ export class CourseFormComponent {
       name: course.name,
       category: course.category,
     });
+  }
+
+  getErrorMessage(fieldName: string){
+    const field = this.form.get(fieldName);
+    if(field?.hasError('required')){
+      return "Campo Obrigatório";
+    }
+
+    if(field?.hasError('minlength')){
+      const requiredLength = this.form.errors ? this.form.errors['minlength']['requiredlenght'] : 3;
+      return `O campo deve ter no mínimo ${requiredLength} caracteres`;
+    }
+    
+    if(field?.hasError('maxlength')){
+      const requiredLength = this.form.errors ? this.form.errors['maxlength']['requiredlenght'] : 3;
+      return `O campo deve ter no máximo ${requiredLength} caracteres`;
+    }
+
+    return "Campo inválido";
   }
 }
